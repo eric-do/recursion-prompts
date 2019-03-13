59837668dfd27f4cd8991ddf3239659337731f12
ericdo <ericdo.617@gmail.com>
2019-03-12 18:43:20 -0700

    Solved #3 - sum nums with nested arrays

--- src/recursion.js
+++ src/recursion.js
@@ -24,6 +24,17 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  if (array.length === 0) { return 0; }
+
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      sum += array[i];
+    } else if (Array.isArray(array[i])) {
+      sum += parseInt(arraySum(array[i]));
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
