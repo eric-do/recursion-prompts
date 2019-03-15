09079b1bc4c8e694318f857e19ccceb2e1cd1c5e
ericdo <ericdo.617@gmail.com>
2019-03-15 12:24:05 -0700

    Solved palindrome()

--- src/recursion.js
+++ src/recursion.js
@@ -113,6 +113,10 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  var str = string.toLowerCase().replace(' ', '');
+  if (str[0] !== str[str.length - 1]) { return false; }
+  if (str.length === 0 || str.length === 1) { return true; }
+  return palindrome(str.substring(1, str.length - 1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
