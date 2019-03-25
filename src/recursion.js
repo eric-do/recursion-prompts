6b1d047d8bd225519183899059bbb5d676a6553d
ericdo <ericdo.617@gmail.com>
2019-03-25 16:58:09 -0700

    Solve compress()

--- src/recursion.js
+++ src/recursion.js
@@ -419,6 +419,12 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+  if (list.length === 0) { return []; }
+  var arr = compress(list.slice(1));
+  if (list[0] !== arr[0]) {
+    arr.unshift(list[0]);
+  }
+  return arr;
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
