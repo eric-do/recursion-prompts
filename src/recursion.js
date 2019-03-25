bf8ebd971ff4a75711a0dceae86b0910317eabf2
ericdo <ericdo.617@gmail.com>
2019-03-25 16:55:18 -0700

    Solved letterTally()

--- src/recursion.js
+++ src/recursion.js
@@ -407,6 +407,10 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+  if (str.length === 0) { return {}; }
+  var obj = letterTally(str.slice(1));
+  obj[str.charAt(0)] = (obj[str.charAt(0)] || 0) + 1;
+  return obj;
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
